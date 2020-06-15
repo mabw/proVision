53fde313057cdb86cd4bcf2435387c954fa1ab55
mabw <uutar@qq.com>
2020-06-15 16:48:36 +0800

    Update the image group styles

--- packages/widgets/imageGroup/imageGroup.style.js
+++ packages/widgets/imageGroup/imageGroup.style.js
@@ -1,12 +1,11 @@
-import React from "react";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-content: center;
+  flex-wrap: wrap;
   img {
     width: ${(props) => 100 / props.columns}%;
-    flex: 1 1 ${(props) => 100 / props.columns}%;
   }
 `;
